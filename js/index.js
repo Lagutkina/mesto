ce8cb83e3a6ea7f878acceb10c00b5d581fc6b0b
Lagutkina Anastasia <lagutkinans@yandex.com>
2022-02-14 19:47:33 +0100

    validation-forms

--- js/index.js
+++ js/index.js
@@ -164,3 +164,97 @@ initialCards.forEach((item) => {
   const card = renderElements(item.name, item.link);
   elements.append(card);
 });
+
+//ВАЛИДАЦИЯ ФОРМ//
+//
+const popupFormConfig = {
+  formSelector: '.popup__form',
+  inputSelector: '.popup__input',
+  submitButtonSelector: '.popup__submit-btn',
+  inactiveButtonClass: 'popup__submit-btn_disabled',
+  inputErrorClass: 'popup__input_type_error',
+  errorClass: 'popup__input-error_active',
+};
+// формы для попапов
+
+//показываем ошибку ввода
+
+const showInputError = (formElement, inputElement, errorMessage) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.add(popupFormConfig.inputErrorClass);
+  errorElement.textContent = errorMessage;
+  errorElement.classList.add(popupFormConfig.errorClass);
+};
+
+//прячем ошибку ввода
+
+const hideInputError = (formElement, inputElement) => {
+  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
+  inputElement.classList.remove(popupFormConfig.inputErrorClass);
+  errorElement.classList.remove(popupFormConfig.errorClass);
+  errorElement.textContent = '';
+};
+
+//проверяем валидность введеного в поле
+
+const checkInputValidity = (formElement, inputElement) => {
+  if (!inputElement.validity.valid) {
+    showInputError(formElement, inputElement, inputElement.validationMessage);
+  } else {
+    hideInputError(formElement, inputElement);
+  }
+};
+
+//проверяем, что НЕ все поля формы валидны
+function hasInvalidInput(inputList) {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid;
+  });
+}
+
+//функция смены состояния кнопки в зависимости от валидности инпутов
+function toggleButtonState(inputList, buttonElement) {
+  if (hasInvalidInput(inputList)) {
+    buttonElement.classList.add(popupFormConfig.inactiveButtonClass);
+    buttonElement.disabled = true;
+  } else {
+    buttonElement.classList.remove(popupFormConfig.inactiveButtonClass);
+    buttonElement.disabled = false;
+  }
+}
+
+//ставим слушатели на инпуты
+
+const setEventListeners = (formElement) => {
+  const inputList = Array.from(
+    formElement.querySelectorAll(popupFormConfig.inputSelector)
+  ); //находим каждый инпут в форме
+  const buttonElement = formElement.querySelector(
+    popupFormConfig.submitButtonSelector
+  ); //находим кнопку сабмит в форме
+  toggleButtonState(inputList, buttonElement); //выключаем кнопку с самого начала ()
+
+  inputList.forEach((inputElement) => {
+    inputElement.addEventListener('input', function() {
+      checkInputValidity(formElement, inputElement); //для каждого инпута проверяем валидность данных
+      toggleButtonState(inputList, buttonElement); //меняем кнопку, если все поля ок
+    });
+  });
+};
+
+// Валидизация всех форм
+
+function enableValidation() {
+  const formList = Array.from(
+    document.querySelectorAll(popupFormConfig.formSelector)
+  );
+  formList.forEach((formElement) => {
+    formElement.addEventListener('submit', (evt) => {
+      evt.preventDefault();
+    });
+
+    setEventListeners(formElement);
+  });
+}
+
+enableValidation();
